cc67171398d18709c173081505dab714e67adabb
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2023-01-28 04:13:33 +0530

    Update pre_init.sh

--- pre_init.sh
+++ pre_init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 echo "###############################################################################"
 echo "#                           KubeKrack                                         #"
 echo "#                 A Vulnerable Kubernetes Lab                                 #"
